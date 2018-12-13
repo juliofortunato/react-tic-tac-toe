03003d2a645ae99da744684a6a1680a39b875382
Júlio Fortunato <juliofortunato58@gmail.com>
2018-12-13 09:58:02 -0200

    Add initial funcionality

--- src/index.js
+++ src/index.js
@@ -3,31 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      value: null
-    }
-  }
-
   render () {
     return (
       <button
         className="square"
-        onClick={() => this.setState({ value: 'X' })}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i}/>;
+  constructor (props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
   }
 
-  render() {
+  handleClick (i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares})
+  }
+
+  renderSquare (i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render () {
     const status = 'Next player: X';
 
     return (
@@ -54,7 +65,7 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  render() {
+  render () {
     return (
       <div className="game">
         <div className="game-board">
